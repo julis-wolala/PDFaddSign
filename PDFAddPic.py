de44211a963530dc3429d2c3ebacd6690ebeccf7
Julis <jun002@e.ntu.edu.sg>
2020-10-07 13:08:10 +0800

    feat
    output to new folder

--- PDFAddPic.py
+++ PDFAddPic.py
@@ -2,8 +2,13 @@
 import os
 import PyPDF2
 
-SignPDF = PyPDF2.PdfFileReader('.\\signPDF\\Sign.pdf')   # 读取签名pdf内容
+#读取签名pdf内容
+SignPDF = PyPDF2.PdfFileReader(r".\signPDF\Sign.pdf")   
 
+sourcepath=r".\outputPDF"
+outputpath = r".\outputPDF"
+
+#定义一个合成原PDF与签名PDF的方法
 def add_sign(Sign_pdf,page_pdf):
     """
     将水印pdf与pdf的一页进行合并
@@ -14,7 +19,8 @@ def add_sign(Sign_pdf,page_pdf):
     page_pdf.mergePage(Sign_pdf.getPage(0))
     return page_pdf
 
-for root, dirs, files in os.walk('.\\sourcePDF', topdown=False):
+#遍历文件夹里的所有文件，然后添加图片签名
+for root, dirs, files in os.walk(r".\sourcePDF", topdown=False):
     for name in files:
         address = os.path.join(root, name)
         print(address)
@@ -26,6 +32,12 @@ for root, dirs, files in os.walk('.\\sourcePDF', topdown=False):
         pdfWriter = PyPDF2.PdfFileWriter()
         pdfWriter.addPage(page_pdf)
 
-        with open(address, 'wb') as target_file:
+        #获取文件名
+        filename=os.path.basename(address)
+        print(filename)
+        #生成输出路径
+        completePosition=os.path.join(outputpath,filename)
+        #把结果保存到新文件夹里
+        with open(completePosition, 'wb') as target_file:
             pdfWriter.write(target_file)
 
